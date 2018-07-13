1d8e2ff6bcd44f912a981154b5bebaa77e17454b
cyrus <cywong@vtc.edu.hk>
2018-07-13 08:48:41 +0800

    Use Timeout for windows and keep signal for Linux.

--- test/unit_test_helper/timeout.py
+++ test/unit_test_helper/timeout.py
@@ -1,22 +1,37 @@
-from functools import wraps
 import errno
 import os
+import platform
 import signal
+from functools import wraps
+from threading import Timer
+from time import sleep
 
 
 def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
     def decorator(func):
-        def _handle_timeout(signum, frame):
+        def _window_handle_timeout():
+            os.kill(os.getpid(), signal.CTRL_C_EVENT)
+
+        def _linux_handle_timeout(signum, frame):
             raise TimeoutError(error_message)
 
         def wrapper(*args, **kwargs):
-            signal.signal(signal.SIGALRM, _handle_timeout)
-            signal.alarm(seconds)
-            try:
-                result = func(*args, **kwargs)
-            finally:
-                signal.alarm(0)
-            return result
+            if platform.system() == "Windows":
+                timer = Timer(seconds, _window_handle_timeout)
+                try:
+                    timer.start()
+                    result = func(*args, **kwargs)
+                finally:
+                    timer.cancel()
+                return result
+            else:
+                signal.signal(signal.SIGALRM, _linux_handle_timeout)
+                signal.alarm(seconds)
+                try:
+                    result = func(*args, **kwargs)
+                finally:
+                    signal.alarm(0)
+                return result
 
         return wraps(func)(wrapper)
 
