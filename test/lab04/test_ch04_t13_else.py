bc7e6be5a01e89395be07cc8b07b10199bb66f8e
Cyrus Wong <cywong@vtc.edu.hk>
2018-09-14 03:55:13 -0400

    Fix test error.

--- test/lab04/test_ch04_t13_else.py
+++ test/lab04/test_ch04_t13_else.py
@@ -5,7 +5,7 @@ from unit_test_helper import is_answer
 if is_answer:
     from lab04.ch04_t13_else_ans import black_knight, french_soldier
 else:
-    from lab04.ch04_t12_else import black_knight, french_soldier
+    from lab04.ch04_t13_else import black_knight, french_soldier
 
 
 class TestOutput(unittest.TestCase):
