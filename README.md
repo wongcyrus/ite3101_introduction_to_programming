d144b72fa4b730508199c4d17be7cfcdc0495d19
Cyrus Wong <cywong@vtc.edu.hk>
2022-08-03 07:51:39 +0000

    Add Windows support.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Python exercises
+
+# Windows
+You need to use create virtual environment as you should not corrupt your Python in Windows.
+```
+python -m venv .venv
+.venv/Scripts/Activate.ps1
+pip install -r requirements.txt
+```
+
+# Codespace
+It is fine to use the Linus Python interpreter directly, as you can delete Codepace and create new one if there is any problem easily.
+```
+pip install -r requirements.txt
+```
+
+![Open in Codespaces](https://classroom.github.com/assets/open-in-codespaces-abfff4d4e15f9e1bd8274d9a39a0befe03a0632bb0f153d0ec72ff541cedbe34.svg)
--- auto_grader.py
+++ auto_grader.py
@@ -10,19 +10,21 @@ from git import Repo
 from config import API_ENDPOINT, API_KEY
 
 PATH_OF_GIT_REPO = r'.git'  # make sure .git folder is properly configured
+sep = os.sep
 
 
 def git_push(message):
     try:
-        shutil.copyfile('lab/autograding.json',
-                        '.github/classroom/autograding.json')
+        shutil.copyfile(os.path.join('lab', 'autograding.json'),
+                        os.path.join('.github', 'classroom', 'autograding.json'))
         repo = Repo(PATH_OF_GIT_REPO)
         repo.git.add(update=True)
         repo.index.commit(message)
         origin = repo.remote(name='origin')
         origin.push()
         print("Commit and push with message: " + message)
-    except BaseException:
+    except BaseException as ex:
+        print(ex)
         print('Some error occured while pushing the code')
 
 
@@ -30,24 +32,24 @@ try:
     argumentList = sys.argv[1:]
     print(argumentList)
 
-    sourceCodeFilePath = "/".join(argumentList[0].split("/")[3:])
+    sourceCodeFilePath = sep.join(argumentList[0].split(sep)[3:])
 
     print(sourceCodeFilePath)
     code = Path(argumentList[0]).read_text()
     print(code)
 
-    source_code_file_path_segments = sourceCodeFilePath.split("/")
+    source_code_file_path_segments = sourceCodeFilePath.split(sep)
     test_code_file_path = os.path.join(
         "tests", source_code_file_path_segments[1], "test_"+source_code_file_path_segments[2])
     print(test_code_file_path)
     retcode = pytest.main(["-x", test_code_file_path])
     if pytest.ExitCode.OK != retcode:
         print("Test Failed!")
-        git_push("[skip actions] " + sourceCodeFilePath)
-        sys.exit(0)
+        git_push("[skip actions] " + sourceCodeFilePath)        
     else:
+        # Azure function is Linux.
         data = {
-            "sourceCodeFilePath": sourceCodeFilePath,
+            "sourceCodeFilePath": sourceCodeFilePath.replace(sep,"/"),
             "sourceCode": code
         }
         print("Calling to Azure function and run test now, please wait.")
@@ -55,7 +57,8 @@ try:
                           headers={"Ocp-Apim-Subscription-Key": API_KEY})
         print(r.status_code)
         print(r.text)
-        git_push(sourceCodeFilePath)
+        if r.text != "Repeated Successful Test.":
+            git_push(sourceCodeFilePath)
 
 except BaseException as ex:
     print(f"Unexpected {ex=}, {type(ex)=}")
